ff896a3cce412217b8ef60d1330ae96d98813855
aaaziz1998 <aaaziz1998@gmail.com>
2020-06-18 21:24:52 +0700

    Initial Commit

---
+++ photo-library-mirai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photo-library-mirai
+//
+//  Created by Achmad Abdul Aziz on 18/06/20.
+//  Copyright © 2020 Achmad Abdul Aziz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
